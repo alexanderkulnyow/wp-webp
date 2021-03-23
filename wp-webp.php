95650e30714c8ada75dd0c8fa3a9650b59e24b68
alexa <alexanderkulnyow@yahoo.com>
2021-03-23 19:38:35 +0300

    desc

---
+++ wp-webp.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* Plugin Name: wp-wepb
+*/
+
+//<picture>
+// <source srcset="mdn-logo-wide.png" media="(min-width: 600px)"> - сколько угодно под условие
+// media - размер экрана
+// type="image/webp" - тип файла
+// <img src="mdn-logo-narrow.png" alt="MDN">
+//</picture>
+ echo 'hello world';
